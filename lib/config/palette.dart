92080b75b9fc49fb6dcb0def6fed69bd93730378
Navin Jongyin <63011210@kmitl.ac.th>
2023-04-05 21:09:11 +0700

    Add palette.dart

---
+++ lib/config/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+  static const Color backgroundColor = Color(0xFF22831);
+  static const Color whiteBackgroundColor = Color(0xFFFFFFFF);
+  static const Color yellowTheme = Color(0xFFFFD369);
+  static const Color greyColor = Color(0xFFC7C7C7);
+}
