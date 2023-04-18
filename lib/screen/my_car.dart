943c5eeabebe32c65cb659bc0a94596a068ccb62
Navin Jongyin <63011210@kmitl.ac.th>
2023-04-18 14:22:11 +0700

    edit my car page

--- lib/screen/my_car.dart
+++ lib/screen/my_car.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:plugspot/config/palette.dart';
+
 class MyCar extends StatefulWidget {
   const MyCar({Key? key}) : super(key: key);
 
@@ -9,6 +12,63 @@ class MyCar extends StatefulWidget {
 class _MyCarState extends State<MyCar> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold() ;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Palette.yellowTheme,
+        title: Text(
+          "My Car",
+          style: GoogleFonts.montserrat(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Palette.backgroundColor,
+          ),
+        ),
+        iconTheme: IconThemeData(color: Palette.backgroundColor),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 60,
+            right: 30,
+            left: 30,
+            child: Container(
+              height: 400,
+              decoration: BoxDecoration(
+                color: Palette.greyColor,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 15),
+                    child: Text("My Car"),
+                  ),
+                  ImageIcon(
+                    AssetImage("images/icon/car.png"),
+                    size: 30,
+                    color: Palette.yellowTheme,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search),
+                        hintText: "Search",
+                        hintStyle: GoogleFonts.audiowide(fontSize: 18)),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    height: 50,
+                    child: FloatingActionButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
