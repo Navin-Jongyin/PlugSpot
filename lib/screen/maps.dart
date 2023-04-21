c02889fe6bb5b46569e012ba0db60fec1c3b1834
Navin Jongyin <63011210@kmitl.ac.th>
2023-04-22 03:04:05 +0700

    edit search bar UI

--- lib/screen/maps.dart
+++ lib/screen/maps.dart
@@ -58,6 +58,7 @@ class MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       key: _scaffoldKey,
       drawer: SideBar(),
       body: Stack(
@@ -118,84 +119,67 @@ class MapSampleState extends State<MapSample> {
             ),
           ),
           Positioned(
-            top: 60,
-            left: 27,
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      _scaffoldKey.currentState?.openDrawer();
+            top: 70,
+            left: 25,
+            right: 25,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              height: 60,
+              decoration: BoxDecoration(
+                  color: Palette.whiteBackgroundColor,
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 3),
+                        blurRadius: 7,
+                        spreadRadius: 3,
+                        color: Palette.greyColor),
+                  ],
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      _scaffoldKey.currentState!.openDrawer();
                     },
-                    backgroundColor: Palette.yellowTheme,
                     child: Icon(
                       Icons.menu,
                       size: 30,
-                      color: Palette.backgroundColor,
+                      color: Palette.yellowTheme,
                     ),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
                   ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  height: 50,
-                  width: 220,
-                  decoration: BoxDecoration(
-                      color: Palette.whiteBackgroundColor,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(0, 2),
-                          blurRadius: 7.0,
-                          spreadRadius: 1,
-                        ),
-                      ]),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextField(
-                        cursorColor: Palette.yellowTheme,
-                        decoration: InputDecoration(
-                          hintText: "Search",
-                          hintStyle: GoogleFonts.montserrat(
-                              fontSize: 18, color: Palette.greyColor),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            size: 30,
-                          ),
+                  VerticalDivider(
+                    color: Palette.greyColor,
+                    thickness: 1.5,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      cursorColor: Palette.yellowTheme,
+                      decoration: InputDecoration(
+                          hintText: 'Search',
+                          prefixIcon: Icon(Icons.search),
                           prefixIconColor: Palette.yellowTheme,
                           border: InputBorder.none,
-                        ),
-                      ),
-                    ],
+                          hintStyle: GoogleFonts.montserrat(
+                              fontSize: 16, color: Palette.greyColor)),
+                      style: GoogleFonts.montserrat(
+                          color: Palette.backgroundColor),
+                    ),
                   ),
-                ),
-                SizedBox(width: 10),
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => QRScanner()));
-                    },
-                    backgroundColor: Palette.yellowTheme,
+                  VerticalDivider(
+                    color: Palette.greyColor,
+                    thickness: 1.5,
+                  ),
+                  InkWell(
                     child: Icon(
                       Icons.qr_code_scanner,
                       size: 30,
                       color: Palette.backgroundColor,
                     ),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                ),
-              ],
+                  )
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
