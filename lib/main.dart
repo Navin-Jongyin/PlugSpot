56a4ec1e8935c55a973c4786da99e0d65df24a6f
Navin Jongyin <63011210@kmitl.ac.th>
2023-04-23 05:26:49 +0700

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:plugspot/config/palette.dart';
 import 'package:plugspot/screen/login_signup.dart';
 import 'package:plugspot/screen/wallet.dart';
