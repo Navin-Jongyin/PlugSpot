559eea09538f9ded4f996e8dcc60c650ceee4e68
Navin Jongyin <63011210@kmitl.ac.th>
2023-05-12 04:27:27 +0700

    edit yellow

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -3,6 +3,6 @@ import 'package:flutter/painting.dart';
 class Palette {
   static const Color backgroundColor = Color(0xFF222831);
   static const Color whiteBackgroundColor = Color(0xFFFFFFFF);
-  static const Color yellowTheme = Color(0xFFFFD369);
+  static const Color yellowTheme = Color.fromARGB(255, 252, 200, 79);
   static const Color greyColor = Color(0xFFC7C7C7);
 }
