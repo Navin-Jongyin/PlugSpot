c7dcb4389bf350790019724c96c0a8270875b272
Navin Jongyin <63011210@kmitl.ac.th>
2023-05-22 15:35:27 +0700

    add myBooking page

--- lib/screen/myBooking.dart
+++ lib/screen/myBooking.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:plugspot/config/palette.dart';
 
 class MyBooking extends StatefulWidget {
   const MyBooking({super.key});
@@ -11,7 +13,25 @@ class _MyBookingState extends State<MyBooking> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Palette.yellowTheme,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Palette.backgroundColor,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          "My Booking",
+          style: GoogleFonts.montserrat(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Palette.backgroundColor),
+        ),
+      ),
     );
   }
 }
